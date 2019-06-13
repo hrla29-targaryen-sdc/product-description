852322165dada0136c04946a4a84603fd087b851
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-06-13 00:24:52 -0700

    Include env-cmd

--- database/Mongo/index.js
+++ database/Mongo/index.js
@@ -3,10 +3,20 @@ require('dotenv').config();
 
 mongoose.set('useFindAndModify', false);
 
+// FOR FEC
 // let DB_URL = (process.env.NODE_ENV === 'development') ? 'mongodb://localhost/productDescription' : `mongodb+srv://matthewmata1030:${process.env.DB_PW}@cluster0-esl5a.mongodb.net/test?retryWrites=true&w=majority`
-let DB_URL = 'mongodb://localhost/productDescription';
+
+// BEFORE AWS
+// let DB_URL = 'mongodb://localhost/productDescription';
+
+//AFTER AWS
+let DB_URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@18.220.135.58/productDescription`;
 
 // Connecting our local storage db to our application with the db name productDescription
 const db = mongoose.connect(DB_URL, {useNewUrlParser: true});
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database!');
+});
 
 module.exports = db;
